b20295843c20b5596bac4bec97eaca2d1c5f0ac8
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-06-26 16:35:11 -0600

    Adds reverse method

---
+++ credit_check.rb
@@ -0,0 +1,21 @@
+card_number = "4929735477250543"
+
+# Your Luhn Algorithm Here
+
+## Turn the numbers into an array?
+numbers = card_number.chars.reverse
+
+
+## Method that selects every other digit from the back
+
+print numbers
+
+## Method that doubles those digits
+
+## Method that sums those digits if over 10
+## Sum all of the digits
+## Module Ten
+
+#Output
+## If it is valid, print "The number is valid!"
+## If it is invalid, print "The number is invalid!"
