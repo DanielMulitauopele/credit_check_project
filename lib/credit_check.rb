23617a7ff4f7af0ddc82ae12834e57b7146b67d5
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-06-28 17:33:10 -0600

    adding instance variables

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,5 +1,24 @@
 class CreditCheck
-  def initialize(card_number)
 
+  def initialize(card_number)
+    @card_number_string = card_number.to_s
   end
+
+  def string_to_array
+    @string_array = @card_number_string.chars
+  end
+
+  def string_array_to_integers
+    @integer_array = []
+    @string_array.each do |integer|
+      new_integer = integer.to_i
+      @integer_array << new_integer
+    end
+    return @integer_array
+  end
+
 end
+
+credit_check = CreditCheck.new("4929735477250543")
+
+print credit_check.string_array_to_integers
